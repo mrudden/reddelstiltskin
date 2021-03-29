851f19749f51094c9dc68b24276e6ba75f23b0b8
Michael Rudden <michael@michaelrudden.com>
2021-03-29 01:58:57 -0400

    code cleanup

--- reddelstiltskin.js
+++ reddelstiltskin.js
@@ -3,22 +3,6 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Anatomy_of_a_WebExtension#background_scripts
 
-// A more luddite approach (doesn't fully work):
-/*
-var presentHostname = window.location.hostname;
-var presentPathname = window.location.pathname;
-//alert(presentURL);
-var agedHostname = presentHostname.replace('www', 'old');
-var agedPathname = presentPathname.replace('\/posts\/', '\/submitted');
-//alert(agedHostname);
-//alert(agedPathname);
-var agedURL = agedHostname;
-agedURL += agedPathname;
-//alert(agedURL);
-//window.location.assign(agedURL);
-*/
-
-
 // match pattern for the URLs to redirect
 var pattern = "*://www.reddit.com/*";
 
